b1cb2dfa9d8f2ff1c2c789b1d25473cf631c6841
michele <spinellimichele91@protonmail.com>
2024-10-21 14:46:54 +0200

    Add project files.

---
+++ ExperimentFabric/ExperimentFabric.Client/Pages/Canvas.razor.js
@@ -0,0 +1,55 @@
+﻿
+//import '/js/fabric.min.js';
+
+
+console.log("1");
+
+let canvasLoaded = false;
+
+var canvas;
+var blueSquare;
+var gradient;
+
+
+console.log("2");
+
+export function initializeFabric() {
+
+    console.log("3");
+    if (typeof fabric === 'undefined') {
+        console.error('Fabric.js is not loaded');
+        return;
+    }
+
+    console.log('Initializing Fabric.js canvas');
+
+    // Initialize a Fabric.js canvas with the given ID
+    var canvasElement = document.getElementById('fabricCanvas');
+    if (!canvasElement) {
+        console.error('Canvas element not found');
+        return;
+    }
+    canvasLoaded = true;
+
+    canvas = new fabric.Canvas('fabricCanvas', {
+        isDrawingMode: true
+    });
+
+    // Create a red square of size 50px by 50px
+    //var redSquare = new fabric.Rect({
+    //    left: 100,
+    //    top: 100,
+    //    fill: 'red',
+    //    width: 50,
+    //    height: 50
+    //});
+
+    //// Add the red square to the canvas
+    //canvas.add(redSquare);
+
+    //canvas.isDrawingMode = true;
+
+    console.log('Red square added to the canvas');
+}
+
+
---
+++ ExperimentFabric/ExperimentFabric.Client/Pages/DrawTest.razor.js
@@ -0,0 +1,88 @@
+﻿
+
+
+//import { Canvas, Rect } from './Pages/fabric.min.js'; // Import fabric.js
+/*import { fabric } from '../wwwroot/js/fabric.min.js';*/
+
+
+var canvas;
+//var canvasLoaded;
+
+//function isDomLoaded() {
+//    return document.querySelector("#fabricCanvas") !== null;
+//}
+
+
+export function initializeCanvas() {
+
+    if (typeof fabric === 'undefined') {
+        console.error('Fabric.js is not loaded');
+        return;
+    }
+
+
+    console.log('Initializing Fabric.js canvas');
+
+
+
+    // Call the initializeFabric function after a slight delay to ensure all scripts are loaded
+    /*setTimeout(initializeFabric, 500);*/
+    //if (!isDomLoaded()) {
+    //    setTimeout(initializeCanvas, 100);
+    //}
+
+
+
+
+    //Initialize a Fabric.js canvas with the given ID
+    var canvasElement = document.getElementById('fabricCanvas');
+    //var canvasElement = document.querySelector('#fabricCanvas');
+    if (!canvasElement) {
+        console.error('Canvas element not found');
+        return;
+    }
+
+
+
+    canvas = new fabric.Canvas('fabricCanvas', {
+        isDrawingMode: false
+    });
+
+
+
+    // Create a red square of size 50px by 50px
+    var redSquare = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 50,
+        height: 50
+    });
+
+    //// Add the red square to the canvas
+    canvas.add(redSquare);
+
+    console.log('Red square added to the canvas');
+
+
+}
+
+export function enableBrush() {
+    canvas.isDrawingMode = true;
+}
+
+
+export function addRectangle() {
+    var rect = new fabric.Rect({
+        left: 200,
+        top: 200,
+        fill: 'blue',
+        width: 60,
+        height: 70,
+    });
+    canvas.add(rect);
+
+    canvas.isDrawingMode = false;
+}
+
+
